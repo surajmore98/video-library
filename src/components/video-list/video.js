a1da2b2f12c20594f4a854fb4f7d9e2971c8e11a
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-05-05 17:39:54 +0530

    #feat - removed like and watchlater code.

--- src/components/video-list/video.js
+++ src/components/video-list/video.js
@@ -1,31 +1,6 @@
 import { useAuth } from "../../contexts/auth-context";
 import { useData } from "../../contexts/data-context";
 import { useNavigate } from 'react-router-dom';
-import axios from "axios";
-import { addToLikedVideos, removeFromLikedVideos } from "../../service/like-video-service";
-
-// add to Liked videolist
-const addToWatchLaterVideos = async (video, token) => {
-    return await axios.post("/api/user/watchlater",
-    {
-        video: video 
-    },
-    {
-        headers: {
-            authorization: token
-        }
-    });
-}
-
-// remove from liked videolist
-const removeFromWatchLaterVideos = async (videoId, token) => {
-    return await axios.delete(`/api/user/watchlater/${videoId}`,
-    {
-        headers: {
-            authorization: token
-        }
-    });
-}
 
 export const Video = ({ data, formStateSetter }) => {
     const { title, creator, image, _id } = data;
@@ -34,55 +9,6 @@ export const Video = ({ data, formStateSetter }) => {
     const navigate = useNavigate();
     const isLiked = likedVideoList.find(x => x._id === _id);
     const isAddedToWatchLater = watchLaterList.find(x => x._id === _id);
-
-    const likeClickHandler = async () => {
-        if(auth && auth.isAuthenticated) {
-            try 
-            {
-                if(!isLiked) {
-                    const response = await addToLikedVideos(data, auth.token);
-                    if(response.status === 201) {
-                        setLikedVideos(response.data.likes);
-                    }                    
-                } else {
-                    const response = await removeFromLikedVideos(data._id, auth.token);
-                    if(response.status === 200) {
-                        setLikedVideos(response.data.likes);
-                    }
-                }
-
-            } catch(e) {
-                console.error(e);
-                setError(e.response.data['errors']);
-            }
-        } else {
-            navigate("/login", {replace: true});
-        }
-    }
-
-    const WatchListClickHandler = async () => {
-        if(auth && auth.isAuthenticated) {
-            try 
-            {
-                if(!isAddedToWatchLater) {
-                    const response = await addToWatchLaterVideos(data, auth.token);
-                    if(response.status === 201) {
-                        setWatchLater(response.data.watchlater);
-                    }                    
-                } else {
-                    const response = await removeFromWatchLaterVideos(data._id, auth.token);
-                    if(response.status === 200) {
-                        setWatchLater(response.data.watchlater);
-                    }
-                }
-            } catch(e) {
-                console.error(e);
-                setError(e.response.data['errors']);
-            }
-        } else {
-            navigate("/login", {replace: true});
-        }
-    }
 
     const playListClickHandler = () => {
         if(auth && auth.isAuthenticated) {
@@ -113,10 +39,10 @@ export const Video = ({ data, formStateSetter }) => {
                     </button>
                 </div>
                 <div className="ml-auto flex">
-                    <button className={`btn btn-round ${isLiked ? 'active' : ''}`} onClick={likeClickHandler}>
+                    <button className={`btn btn-round ${isLiked ? 'active' : ''}`}>
                         <i className={`material-icons ${isLiked ? 'active' : ''}`} title="like">thumb_up</i>
                     </button>
-                    <button className={`btn btn-round ${isAddedToWatchLater ? 'active' : ''}`} onClick={WatchListClickHandler}>
+                    <button className={`btn btn-round ${isAddedToWatchLater ? 'active' : ''}`}>
                         <i className={`material-icons ${isAddedToWatchLater ? 'active' : ''}`} title="add to watch later">watch_later</i>
                     </button>
                     <button className="btn btn-round" onClick={playListClickHandler}>
