58ac8a15e6b517e4c0c47171ef6eb9a56e61380d
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-05-09 22:40:33 +0530

    #feat - routes added.

---
+++ src/pages/pages.js
@@ -0,0 +1,15 @@
+import { Home } from "./home";
+import { VideoList } from "./video-list";
+import { WatchLater } from "./watch-later";
+import { Login } from "./login";
+import { Register } from "./register";
+import { Logout } from "./logout";
+import { PlayList } from "./playlist";
+import { PlayListDetails } from "./play-list-detail";
+import { LikedVideoList } from "./liked-video-list";
+import { History } from "./history";
+
+export  {
+    WatchLater, Login, Register, Logout, PlayList, PlayListDetails, LikedVideoList,
+    History, Home, VideoList
+}
