cae88ab747ef3bcd4f0b5158c0da587e716fafc6
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-05-09 22:35:23 +0530

    #feat - playlist detail page added.

--- src/pages/play-list-detail.js
+++ src/pages/play-list-detail.js
@@ -1,20 +1,21 @@
 import { useParams } from "react-router-dom";
 import { Navbar } from "../components/nav-bar";
+import { NoItems } from "../components/no-items";
 import { PlaylistItem } from "../components/play-list-item";
 import { useAuth } from "../contexts/auth-context";
 import { useData } from "../contexts/data-context";
 import { removeVideoFromPlaylist } from "../service/play-list-service";
+import { ResponseCode } from "../utility/status-code";
 
 export const PlayListDetails = () => {
     const { playList, setPlayListData  } = useData();
-    const { user, auth } = useAuth();
+    const { auth } = useAuth();
     const { id } = useParams();
-    const currentUser = `${user.firstName} ${user.lastName}`; 
     const currentPlayList = playList.find(x => x._id === id);
 
     const removeVideo = async (videoId) => {
         const response = await removeVideoFromPlaylist(id, videoId, auth.token);
-        if(response.status === 200) {
+        if(response.status === ResponseCode.OK) {
             await setPlayListData(auth.token);
         }
     }
@@ -35,16 +36,13 @@ export const PlayListDetails = () => {
                     <div className="m-md font-lg charcoal-gray align-self-start">
                         <span>{ currentPlayList.videos.length } videos, Updated { currentPlayList.updateDate }</span>
                     </div>
-                    <div className="flex flex-start m-md mt-xl pt-xl border-top-black">
-                        <div className="avatar avatar-letter avatar-md bg-charcoal-gray">{currentUser.charAt(0)}</div>
-                        <div className="font-bold font-lg">{currentUser}</div>
-                    </div>
                 </div>
                 <div className="bg-charcoal-white p-xl pt-0 vertical-flow-auto">
-                    {
-                        currentPlayList && 
-                        currentPlayList.videos.map((video) => <PlaylistItem video={video} deleteVideoHandler={removeVideo} />)
-                    }
+                {
+                    (currentPlayList && currentPlayList.length) ?
+                        currentPlayList.videos.map((video, index) => <PlaylistItem video={video} deleteVideoHandler={removeVideo} key={index} />)
+                        : <NoItems/>
+                }
                 </div>
             </div>         
         </div>
